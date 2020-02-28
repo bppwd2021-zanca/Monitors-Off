5d72ed2b55d964a6a13085277a451e38f2bbffd9
bppwd2021-zanca <l.zanca@baypath.net>
2020-02-28 09:37:21 -0500

    albin doesnt really care

--- src/com/company/ProgressBar.java
+++ src/com/company/ProgressBar.java
@@ -1,4 +1,16 @@
 package com.company;
+import java.awt.*;
 public class ProgressBar{
-    
+    private int x,y,width,height,progress;
+    public ProgressBar(int x,int y,int width,int height){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+        progress=0;
+    }public void draw(Graphics pen){
+        pen.setColor(Color.white);
+        pen.fillRect(x,y,width,height);
+        pen.fillRect(x+15,y+15,,height-30);
+    }
 }
